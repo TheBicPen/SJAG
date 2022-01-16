f29c82944286809dd2f13b264743c0d808b14806
Alex <mashkal2000@gmail.com>
2022-01-15 22:47:26 -0800

    Create proper Docker build. Clean up App spaghetti.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
-FROM maven:3.6.3-openjdk-16
 
-WORKDIR /root/.m2/repository
-COPY . ./
-RUN mvn verify clean --fail-never
-RUN mvn compile
-ENTRYPOINT [ "mvn","exec:java" ]
+# Build image
+FROM maven:3.6.3-openjdk-16 AS Build
+COPY src /src
+COPY pom.xml /
+RUN mvn clean package
+
+# Execution image
+FROM openjdk:16
+COPY --from=Build /target/ApiGateway-1.0.jar /
+ENTRYPOINT [ "java", "-jar", "/ApiGateway-1.0.jar" ]
 EXPOSE 8000
--- src/main/java/io/github/thebicpen/App.java
+++ src/main/java/io/github/thebicpen/App.java
@@ -13,43 +13,32 @@ public class App {
 
   public static void main(String[] args) throws IOException {
     HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", IN_PORT), 0);
-    if (args.length == 0) {
-      System.out.printf("Using default input port %i\n", IN_PORT);
-      System.out.printf("Using default output port %i\n", OUT_PORT);
-    } else if (args.length == 1) {
-      try {
-        IN_PORT = Integer.parseInt(args[0]);
-        System.out.printf("Using default output port %i\n", OUT_PORT);
-      } catch (NumberFormatException e) {
-        System.err.printf("Unable to parse input port: %i\n", args[0]);
-        System.exit(1);
-      }
-    } else {
+    Map<String, Endpoint> services = new HashMap<>();
+    if (args.length >= 2) {
       try {
         IN_PORT = Integer.parseInt(args[0]);
         OUT_PORT = Integer.parseInt(args[1]);
       } catch (NumberFormatException e) {
-        System.err.printf("Unable to parse port numbers: %i, %i\n", args[0], args[1]);
+        System.err.printf("Unable to parse port numbers: %d, %d\n", args[0], args[1]);
         System.exit(1);
       }
-    }
-
-    Map<String, Endpoint> services = new HashMap<>();
-    for (String arg : Arrays.copyOfRange(args, 2, args.length)) {
-      String[] item = arg.split(",");
-      if (item.length >= 2) {
-        try {
-          int port = item.length == 2 ? OUT_PORT : Integer.parseInt(item[2]);
-          services.put(item[0], new Endpoint(port, item[1]));
-          System.out.printf("Mapping %s -> %s (port %i)\n", item[0], item[1], port);
-        } catch (NumberFormatException e) {
-          System.err.println("Unable to parse port number: " + item[2]);
-          System.exit(1);
+      for (String arg : Arrays.copyOfRange(args, 2, args.length)) {
+        String[] item = arg.split(",");
+        if (item.length >= 2) {
+          try {
+            int port = item.length == 2 ? OUT_PORT : Integer.parseInt(item[2]);
+            services.put(item[0], new Endpoint(port, item[1]));
+            System.out.printf("Mapping %s -> %s (port %d)\n", item[0], item[1], port);
+          } catch (NumberFormatException e) {
+            System.err.println("Unable to parse port number: " + item[2]);
+            System.exit(1);
+          }
         }
       }
     }
     server.createContext("/", new Gateway(services, OUT_PORT));
     server.start();
-    System.out.printf("API Gateway server started on port %d...\n", IN_PORT);
+    System.out.printf("Routing requests to port %d\n", OUT_PORT);
+    System.out.printf("API Gateway listening on port %d...\n", IN_PORT);
   }
 }
