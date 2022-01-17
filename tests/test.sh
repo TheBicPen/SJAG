fd0df0891efebd5932d04835d6770de0caff86a3
Alex <mashkal2000@gmail.com>
2022-01-16 19:06:43 -0800

    Fix script using wrong location

--- tests/test.sh
+++ tests/test.sh
@@ -1,4 +1,4 @@
 set -e  # exit on any command failing
 
-docker-compose -f docker-compose.test.yml up --build --remove-orphans --exit-code-from runner
+docker-compose -f "$(dirname $0)/docker-compose.test.yml" up --build --remove-orphans --exit-code-from runner
 
